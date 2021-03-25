d668c98524fb02f9ac68bff30556ba9781aa8f68
Trần Quang Hùng <tranquang.hung@etc.vn>
2021-03-25 14:56:56 +0700

    Perform ESlint

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import PropTypes from 'prop-types';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+    Component: PropTypes.func.isRequired,
+    pageProps: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
+
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Home</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </>
+    );
 }
