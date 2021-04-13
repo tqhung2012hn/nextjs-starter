a833fc0ddf995991078328d0e7b02953e33b353c
Trần Quang Hùng <tranquang.hung@etc.vn>
2021-04-13 15:45:07 +0700

    Configure the character encoding and the viewport

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 
 import { AuthProvider } from '@/contexts/Auth';
 import { GlobalStyles, theme } from '@/styles/GlobalStyles';
@@ -7,6 +8,13 @@ import { GlobalStyles, theme } from '@/styles/GlobalStyles';
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
             <GlobalStyles />
             <ThemeProvider theme={theme}>
                 <AuthProvider>
